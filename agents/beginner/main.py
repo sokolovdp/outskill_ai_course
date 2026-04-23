1313868cf7f1efc87fce10af4f320db673887fca
Ishan <ishandutta0098@gmail.com>
2026-04-23 22:20:54 +0530

    :recycle: [agents/beginner] switch default test input and disable verbose mode

--- agents/beginner/main.py
+++ agents/beginner/main.py
@@ -8,13 +8,14 @@ from tasks import hate_speech_detection_task
 # Define the Crew with agents and tasks
 crew = Crew(
     agents=[hate_speech_detector],
-    tasks=[hate_speech_detection_task]
+    tasks=[hate_speech_detection_task],
+    verbose=False,
     )
 
 # Kickoff the Crew with the input query
-Text = "Public libraries are important resources for everyone in the city."
+# Text = "Public libraries are important resources for everyone in the city."
 # Text = "That country is less intelligent and incapable of contributing to society."
-# Text = "People from that race are all untrustworthy and should not be allowed to work in public jobs."
+Text = "People from that race are all untrustworthy and should not be allowed to work in public jobs."
 
 result = crew.kickoff(inputs={"text": Text})
 
